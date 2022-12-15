affc4ed4c9ac7e8de09062dfe643ea31f5227d03
Ashish7520 <ashishnandwana2@gmail.com>
2022-12-15 22:55:17 +0530

    create nodes and modify doms

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//quetySelector
+/*quetySelector
 
 var items=document.querySelector('.list-group-item:nth-child(2)');
 console.log(items)
@@ -18,5 +18,14 @@ for(i=0; i<odd.length; i++){
     odd[i].style.backgroundColor='green'
 }
 
-/* queryselector do impact on first element among all element
+ queryselector do impact on first element among all element
 while queryselectorall impact on all element */
+var newDiv=document.createElement('div')
+console.log(newDiv)
+
+var newDivText=document.createTextNode('Hello')
+newDiv.appendChild(newDivText)
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1')
+container.insertBefore(newDiv, h1)
